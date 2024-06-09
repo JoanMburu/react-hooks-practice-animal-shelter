e70f343f261b4a7c7f4f6e9dde4d133969905542
JoanMburu <joan.wangui@student.moringaschool.com>
2024-06-09 19:13:08 +0300

    update animal shelter

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,39 @@
-import React, { useState } from "react";
-
+import React, { useState, useEffect, useCallback } from "react";
 import Filters from "./Filters";
 import PetBrowser from "./PetBrowser";
 
 function App() {
   const [pets, setPets] = useState([]);
   const [filters, setFilters] = useState({ type: "all" });
+
+  const onChangeType = (newType) => {
+    setFilters({ type: newType });
+  };
+
+  const fetchPets = useCallback(() => {
+    let url = "http://localhost:3001/pets";
+    if (filters.type !== "all") {
+      url += `?type=${filters.type}`;
+    }
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setPets(data))
+      .catch((error) => console.error("Error fetching pets:", error));
+  }, [filters.type]); 
+
+  useEffect(() => {
+    fetchPets();
+  }, [fetchPets]); 
+
+  const onAdoptPet = (id) => {
+    const updatedPets = pets.map((pet) => {
+      if (pet.id === id) {
+        return { ...pet, isAdopted: true };
+      }
+      return pet;
+    });
+    setPets(updatedPets);
+  };
 
   return (
     <div className="ui container">
@@ -15,10 +43,10 @@ function App() {
       <div className="ui container">
         <div className="ui grid">
           <div className="four wide column">
-            <Filters />
+            <Filters onChangeType={onChangeType} onFindPetsClick={fetchPets} />
           </div>
           <div className="twelve wide column">
-            <PetBrowser />
+            <PetBrowser pets={pets} onAdoptPet={onAdoptPet} />
           </div>
         </div>
       </div>
--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,20 +1,34 @@
 import React from "react";
 
-function Filters() {
+function Filters({ onChangeType, onFindPetsClick }) {
+  const handleChange = (event) => {
+    onChangeType(event.target.value);
+  };
+
+  const handleClick = () => {
+    onFindPetsClick();
+  };
+
   return (
     <div className="ui form">
       <h3>Animal type</h3>
       <div className="field">
-        <select name="type" id="type" aria-label="type">
+        <select
+          name="type"
+          id="type"
+          aria-label="type"
+          onChange={handleChange}
+        >
           <option value="all">All</option>
           <option value="cat">Cats</option>
           <option value="dog">Dogs</option>
           <option value="micropig">Micropigs</option>
         </select>
       </div>
-
       <div className="field">
-        <button className="ui secondary button">Find pets</button>
+        <button className="ui secondary button" onClick={handleClick}>
+          Find pets
+        </button>
       </div>
     </div>
   );
